70fe2727d78f539525fe73ecfca4c5fdc3f9f7ea
Nicolas Loontjens <nicolas.loontjens@student.howest.be>
2022-11-12 11:37:17 +0100

    change starting route

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/home",
+      initialRoute: "/register",
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
