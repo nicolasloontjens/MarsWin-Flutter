2048016996e9dd5c2a57888b89e47ba253b13f1b
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2022-12-03 14:03:25 +0100

    wallet overview layout done

--- lib/pages/walletOverview.dart
+++ lib/pages/walletOverview.dart
@@ -1,3 +1,4 @@
+import 'package:carbon_icons/carbon_icons.dart';
 import "package:flutter/material.dart";
 
 class WalletOverview extends StatefulWidget {
@@ -16,7 +17,77 @@ class _WalletOverviewState extends State<WalletOverview> {
           decoration: BoxDecoration(color: Color(0xFFF1EBE6)),
           width: MediaQuery.of(context).size.width,
           child: Column(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Wallet",
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "Nasalization")),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(CarbonIcons.currency),
+                  Text('  1000',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'Nasalization'))
+                ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.only(top: 25),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width * 0.40,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFE87470),
+                        border: Border.all(color: Colors.black, width: 2.0),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                        child: Text(
+                      'Add funds',
+                      style: TextStyle(
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontSize: 16),
+                    )),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 25),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 40,
+                    width: MediaQuery.of(context).size.width * 0.40,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFE87470),
+                        border: Border.all(color: Colors.black, width: 2.0),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Center(
+                        child: Text(
+                      'Withdraw funds',
+                      style: TextStyle(
+                          fontFamily: "Inter",
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontSize: 16),
+                    )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.25,
+              )
+            ],
           ),
         ),
       ),
