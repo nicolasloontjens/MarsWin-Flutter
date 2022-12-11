1ea8dddfd11e8b9a1c6fc2167940df4a947ff8b5
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2022-12-11 15:19:52 +0100

    remove test player

--- lib/pages/standings.dart
+++ lib/pages/standings.dart
@@ -2,7 +2,6 @@ import "package:flutter/material.dart";
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:marswin/data/network/datafetcher.dart';
 import 'package:marswin/data/network/types/Championship.dart';
-import 'package:lecle_yoyo_player/lecle_yoyo_player.dart';
 
 class StandingsPage extends StatefulWidget {
   const StandingsPage({Key? key}) : super(key: key);
@@ -34,66 +33,6 @@ class _StandingsPageState extends State<StandingsPage> {
           decoration: BoxDecoration(color: Color(0xFFF1EBE6)),
           child: Column(
             children: [
-              YoYoPlayer(
-                aspectRatio: 16 / 9,
-                url:
-                    // 'https://dsqqu7oxq6o1v.cloudfront.net/preview-9650dW8x3YLoZ8.webm',
-                    // "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-                    // "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-                    "http://172.104.247.19/hls/race.m3u8",
-                allowCacheFile: true,
-                onCacheFileCompleted: (files) {
-                  print('Cached file length ::: ${files?.length}');
-
-                  if (files != null && files.isNotEmpty) {
-                    for (var file in files) {
-                      print('File path ::: ${file.path}');
-                    }
-                  }
-                },
-                onCacheFileFailed: (error) {
-                  print('Cache file error ::: $error');
-                },
-                videoStyle: const VideoStyle(
-                  qualityStyle: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                  forwardAndBackwardBtSize: 30.0,
-                  playButtonIconSize: 40.0,
-                  playIcon: Icon(
-                    Icons.add_circle_outline_outlined,
-                    size: 40.0,
-                    color: Colors.white,
-                  ),
-                  pauseIcon: Icon(
-                    Icons.remove_circle_outline_outlined,
-                    size: 40.0,
-                    color: Colors.white,
-                  ),
-                  videoQualityPadding: EdgeInsets.all(5.0),
-                ),
-                videoLoadingStyle: VideoLoadingStyle(
-                  loading: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        SizedBox(height: 16.0),
-                        Text("Loading video..."),
-                      ],
-                    ),
-                  ),
-                ),
-                onFullScreen: (value) {
-                  setState(() {
-                    if (fullscreen != value) {
-                      fullscreen = value;
-                    }
-                  });
-                },
-              ),
               FutureBuilder(
                 future: _championship,
                 builder: (context, snapshot) {
