80454cecc44ef84b7575901f81b561d5f2a3ea99
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2022-12-12 15:53:15 +0100

    add pull to refresh list

--- lib/pages/raceOverview.dart
+++ lib/pages/raceOverview.dart
@@ -1,7 +1,9 @@
+import 'package:carbon_icons/carbon_icons.dart';
 import "package:flutter/material.dart";
 import 'package:marswin/data/network/datafetcher.dart';
 import 'package:marswin/data/network/types/Race.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:intl/intl.dart';
 
 class RaceOverviewPage extends StatefulWidget {
   const RaceOverviewPage({Key? key}) : super(key: key);
@@ -21,6 +23,13 @@ class _RaceOverviewPageState extends State<RaceOverviewPage> {
     super.initState();
     finishedRaces = Datafetcher.getFinishedRaces();
     plannedRaces = Datafetcher.getPlannedRaces();
+  }
+
+  Future refresh() async {
+    setState(() {
+      finishedRaces = Datafetcher.getFinishedRaces();
+      plannedRaces = Datafetcher.getPlannedRaces();
+    });
   }
 
   @override
@@ -44,6 +53,8 @@ class _RaceOverviewPageState extends State<RaceOverviewPage> {
                 selectedBorderColor: Colors.red[700],
                 selectedColor: Colors.white,
                 fillColor: Colors.red[400],
+                borderWidth: 2,
+                borderColor: Colors.black,
                 color: Colors.black,
                 constraints: BoxConstraints(
                     minWidth: MediaQuery.of(context).size.width * 0.29,
@@ -81,56 +92,97 @@ class _RaceOverviewPageState extends State<RaceOverviewPage> {
                         future: finishedRaces,
                         builder: ((context, snapshot) {
                           if (snapshot.hasData) {
-                            return ListView.builder(
-                                scrollDirection: Axis.vertical,
-                                shrinkWrap: true,
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: ((context, index) {
-                                  return Container(
-                                    margin: EdgeInsets.symmetric(
-                                        horizontal: 50, vertical: 10),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        border: Border.all(
-                                            color: Colors.black, width: 2)),
-                                    child: ListTile(
-                                        title: Text(
-                                          snapshot.data![index].name,
-                                          style: TextStyle(
-                                              fontSize: 24,
+                            return RefreshIndicator(
+                              onRefresh: () async {
+                                await refresh();
+                              },
+                              child: ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  shrinkWrap: true,
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder: ((context, index) {
+                                    snapshot.data!.sort(
+                                        (a, b) => b.date.compareTo(a.date));
+                                    return Container(
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: 45, vertical: 12),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          boxShadow: [
+                                            BoxShadow(
                                               color: Colors.black,
-                                              fontWeight: FontWeight.w500,
-                                              fontFamily: 'Inter'),
-                                        ),
-                                        onTap: () {
-                                          int id = snapshot.data![index].id;
-                                          Navigator.pushNamed(
-                                              context, '/race/$id');
-                                        },
-                                        subtitle: Text(
-                                            snapshot.data![index].date
-                                                .toString(),
+                                              blurRadius: 0,
+                                              spreadRadius: 3.5,
+                                              offset: Offset(1, 1),
+                                            ),
+                                            BoxShadow(
+                                              color: Color(0xFFF1EBE6),
+                                              blurRadius: 0,
+                                              offset: Offset(0, 0),
+                                            )
+                                          ]),
+                                      child: ListTile(
+                                          title: Text(
+                                            snapshot.data![index].name,
                                             style: TextStyle(
-                                                fontSize: 16,
-                                                fontFamily: 'Inter',
-                                                color: Colors.black87,
-                                                fontWeight: FontWeight.w400)),
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(0.5)),
-                                        contentPadding: EdgeInsets.all(0.5),
-                                        leading: Container(
-                                          width: 100,
-                                          height: 100,
-                                          decoration: BoxDecoration(
-                                              image: const DecorationImage(
-                                            fit: BoxFit.fitHeight,
-                                            image: AssetImage(
-                                                "assets/images/marswin-no-text.png"),
+                                                fontSize: 20,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.w500,
+                                                fontFamily: 'Inter'),
+                                          ),
+                                          trailing: GestureDetector(
+                                            onTap: () {
+                                              int id = snapshot.data![index].id;
+                                              Navigator.pushNamed(
+                                                  context, '/race/$id');
+                                            },
+                                            child: Container(
+                                              margin:
+                                                  EdgeInsets.only(right: 20),
+                                              child: Column(
+                                                children: [
+                                                  Icon(
+                                                    CarbonIcons
+                                                        .information_square,
+                                                    size: 30,
+                                                    color: Colors.black,
+                                                  ),
+                                                  Text(
+                                                    'View results',
+                                                    style: TextStyle(
+                                                        fontFamily: 'Inter'),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                              DateFormat('yyyy-MM-dd – kk:mm')
+                                                  .format(snapshot
+                                                      .data![index].date),
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontFamily: 'Inter',
+                                                  color: Colors.black87,
+                                                  fontWeight: FontWeight.w400)),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(0.5)),
+                                          contentPadding: EdgeInsets.all(0.5),
+                                          leading: Container(
+                                            width: 100,
+                                            height: 100,
+                                            decoration: BoxDecoration(
+                                                image: const DecorationImage(
+                                              fit: BoxFit.fitHeight,
+                                              image: AssetImage(
+                                                  "assets/images/marswin-no-text.png"),
+                                            )),
                                           )),
-                                        )),
-                                  );
-                                }));
+                                    );
+                                  })),
+                            );
                           } else {
                             return Center(
                                 child: SpinKitWave(
@@ -147,56 +199,96 @@ class _RaceOverviewPageState extends State<RaceOverviewPage> {
                         future: plannedRaces,
                         builder: ((context, snapshot) {
                           if (snapshot.hasData) {
-                            print(snapshot.data);
-                            return ListView.builder(
-                                scrollDirection: Axis.vertical,
-                                shrinkWrap: true,
-                                itemCount: snapshot.data!.length,
-                                itemBuilder: ((context, index) {
-                                  return Container(
-                                    margin: EdgeInsets.symmetric(
-                                        horizontal: 50, vertical: 10),
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        border: Border.all(
-                                            color: Colors.black, width: 2)),
-                                    child: ListTile(
-                                        title: Text(
-                                          snapshot.data![index].name,
-                                          style: TextStyle(
-                                              fontSize: 24,
+                            return RefreshIndicator(
+                              onRefresh: () async {
+                                await refresh();
+                              },
+                              child: ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  shrinkWrap: true,
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder: ((context, index) {
+                                    snapshot.data!.sort(
+                                        (a, b) => b.date.compareTo(a.date));
+                                    return Container(
+                                      margin: EdgeInsets.symmetric(
+                                          horizontal: 45, vertical: 12),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          boxShadow: [
+                                            BoxShadow(
                                               color: Colors.black,
-                                              fontWeight: FontWeight.w500,
-                                              fontFamily: 'Inter'),
-                                        ),
-                                        onTap: () {
-                                          Navigator.pushNamed(
-                                              context, '/races/live');
-                                        },
-                                        subtitle: Text(
-                                            snapshot.data![index].date
-                                                .toString(),
+                                              blurRadius: 0,
+                                              spreadRadius: 3.5,
+                                              offset: Offset(1, 1),
+                                            ),
+                                            BoxShadow(
+                                              color: Color(0xFFF1EBE6),
+                                              blurRadius: 0,
+                                              offset: Offset(0, 0),
+                                            )
+                                          ]),
+                                      child: ListTile(
+                                          title: Text(
+                                            snapshot.data![index].name,
                                             style: TextStyle(
-                                                fontSize: 16,
-                                                fontFamily: 'Inter',
-                                                color: Colors.black87,
-                                                fontWeight: FontWeight.w400)),
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(0.5)),
-                                        contentPadding: EdgeInsets.all(0.5),
-                                        leading: Container(
-                                          width: 100,
-                                          height: 100,
-                                          decoration: BoxDecoration(
-                                              image: const DecorationImage(
-                                            fit: BoxFit.fitHeight,
-                                            image: AssetImage(
-                                                "assets/images/marswin-no-text.png"),
+                                                fontSize: 20,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.w500,
+                                                fontFamily: 'Inter'),
+                                          ),
+                                          trailing: GestureDetector(
+                                            onTap: () {
+                                              Navigator.pushNamed(
+                                                  context, '/races/live');
+                                            },
+                                            child: Container(
+                                              margin:
+                                                  EdgeInsets.only(right: 20),
+                                              child: Column(
+                                                children: [
+                                                  Icon(
+                                                    CarbonIcons
+                                                        .information_square,
+                                                    size: 30,
+                                                    color: Colors.black,
+                                                  ),
+                                                  Text(
+                                                    'View results',
+                                                    style: TextStyle(
+                                                        fontFamily: 'Inter'),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                              DateFormat('yyyy-MM-dd – kk:mm')
+                                                  .format(snapshot
+                                                      .data![index].date),
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontFamily: 'Inter',
+                                                  color: Colors.black87,
+                                                  fontWeight: FontWeight.w400)),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(0.5)),
+                                          contentPadding: EdgeInsets.all(0.5),
+                                          leading: Container(
+                                            width: 100,
+                                            height: 100,
+                                            decoration: BoxDecoration(
+                                                image: const DecorationImage(
+                                              fit: BoxFit.fitHeight,
+                                              image: AssetImage(
+                                                  "assets/images/marswin-no-text.png"),
+                                            )),
                                           )),
-                                        )),
-                                  );
-                                }));
+                                    );
+                                  })),
+                            );
                           } else {
                             return Center(
                                 child: SpinKitWave(
