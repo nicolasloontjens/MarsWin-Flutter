7ad257f0402c26efd1d86746df34df519f940f4a
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2022-12-14 14:29:03 +0100

    update betoverview

--- lib/pages/betOverview.dart
+++ lib/pages/betOverview.dart
@@ -1,7 +1,10 @@
+import 'package:carbon_icons/carbon_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:marswin/data/network/datafetcher.dart';
 import 'package:marswin/data/network/types/Bet.dart';
+import 'package:marswin/data/network/types/BetResponse.dart';
 
 class BetOverview extends StatefulWidget {
   @override
@@ -9,7 +12,7 @@ class BetOverview extends StatefulWidget {
 }
 
 class _BetOverviewState extends State<BetOverview> {
-  late Future<List<Bet>> bets;
+  late Future<List<BetResponse>> bets;
 
   @override
   void initState() {
@@ -20,26 +23,117 @@ class _BetOverviewState extends State<BetOverview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: FutureBuilder<List<Bet>>(
-        future: bets,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data![index].raceId.toString()),
-                  subtitle: Text(snapshot.data![index].driverId.toString()),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        },
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color(0xFFE87470),
+        title: Text('Your bet history: ',
+            style: TextStyle(
+                fontFamily: 'Nasalization',
+                fontSize: 24,
+                fontWeight: FontWeight.w400)),
+        automaticallyImplyLeading: false,
+        leadingWidth: 100,
+        leading: ElevatedButton.icon(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(CarbonIcons.undo),
+          label: Text('', style: TextStyle(fontFamily: 'Inter')),
+          style: ElevatedButton.styleFrom(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              textStyle: TextStyle(
+                  fontFamily: 'Nasalization',
+                  fontSize: 32,
+                  fontWeight: FontWeight.w500),
+              animationDuration: Duration(milliseconds: 0)),
+        ),
       ),
-    ));
+      body: Container(
+        decoration: BoxDecoration(
+            color: Color(0xFFFFF9F3),
+            border: Border(top: BorderSide(color: Colors.black, width: 1))),
+        child: FutureBuilder<List<BetResponse>>(
+          future: bets,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 0,
+                          spreadRadius: 3.5,
+                          offset: Offset(1, 1),
+                        ),
+                        BoxShadow(
+                          color: Color(0xFFE75657),
+                          blurRadius: 0,
+                          offset: Offset(0, 0),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(5),
+                      color: Color(0xFFFFF9F3)),
+                  child: ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+                        margin: EdgeInsets.only(bottom: 5),
+                        decoration: BoxDecoration(
+                            color: Color(0xFFfaeee7),
+                            border: Border(
+                                top: BorderSide(color: Colors.black),
+                                bottom: BorderSide(color: Colors.black))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              child: Text(
+                                  "${snapshot.data![index].driver.name}",
+                                  style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w500)),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Icon(CarbonIcons.currency, size: 24),
+                                Text(
+                                  "${snapshot.data![index].bet.amount}",
+                                  style: TextStyle(
+                                      fontFamily: 'Inter',
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w500),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                      // ListTile(
+                    },
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return Center(
+                child: SpinKitWave(
+              color: Colors.black,
+              size: 40,
+              itemCount: 6,
+            ));
+          },
+        ),
+      ),
+    );
   }
 }
